0561e4bddf5dd7c3578566d27d701f764c25be19
Richard Keller <richard@briefmedia.com>
2017-07-06 16:16:37 -0500

    himalaya-example works, run casperjs himalaya-example.js

--- himalaya-example.js
+++ himalaya-example.js
@@ -2,17 +2,18 @@ var casper = require('casper').create();
 var himalaya = require('himalaya')
 var fs = require('fs')
 
-casper.start('https://www.cliniciansbrief.com/article/echinococcus-spp-tapeworms-dogs-cats', function() {
+// casper.start('https://www.cliniciansbrief.com/article/echinococcus-spp-tapeworms-dogs-cats', function() {
+casper.start('http://localhost:81/node/1', function() {
 	var body_html;
 	 body_html = this.evaluate(function() {
-	 		return document.querySelector('#content .container').innerHTML
+	 		// return document.querySelector('#content .container').innerHTML
 	    return document.body.innerHTML;
 	 });
   body_html = body_html.replace(/\r/g, '')
   body_html = body_html.replace(/\n/g, '')
   body_html = body_html.replace(/\t/g, '')
 	var json = himalaya.parse(body_html)
-	fs.write('./himalaya-output.json', JSON.stringify(json, null, 4), function(err){})
+	fs.write('./himalaya-output-NEW.json', JSON.stringify(json, null, 4), function(err){})
 });
 
 casper.run();
